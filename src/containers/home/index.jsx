c1b318edf78770ab3927b6dbfdffeddb5d60ed2f
achraf <achraf04chke@gmail.com>
2025-03-24 13:56:30 +0000

    feat: add social media links to home page
    - Added Instagram, LinkedIn and GitHub social icons below hire button
    - Implemented animation effect for social links section using react-simple-animate
    - Added responsive styling for social icons across different screen sizes
    - Added necessary CSS for hover effects on social links
    - Fixed media query issues in existing styles

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -4,11 +4,7 @@ import { Animate } from 'react-simple-animate'
 import './styles.scss';
 
 const Home = () => {
-
-
     const navigate = useNavigate();
-
-
     const handleNavigateToContactMePage = () => {
         navigate("/contact")
     }
@@ -20,9 +16,8 @@ const Home = () => {
                         Hello, I'm Achraf <br />
                         Full-stack developer
                     </h1>
-
                 </div>
-                <Animate 
+                <Animate
                 play
                 duration={1.5}
                 delay={1}
@@ -31,20 +26,38 @@ const Home = () => {
                 }}
                 end={{
                     transform:"translatex(0px)",
-
                 }}>
                     <div className="home__contact-me">
                         <button onClick={handleNavigateToContactMePage}>Hire Me</button>
-
                     </div>
-
                 </Animate>
-
-
+                <Animate
+                play
+                duration={1.5}
+                delay={1.5}
+                start={{
+                    transform:"translateY(550px)",
+                    opacity: 0
+                }}
+                end={{
+                    transform:"translateY(0px)",
+                    opacity: 1
+                }}>
+                    <div className="home__social">
+                        <a href="https://www.instagram.com/achraf_chkr/" className="home__social-icon" target="_blank" rel="noreferrer">
+                            <i className="uil uil-instagram"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/achraf-chikrabane-850554291/" className="home__social-icon" target="_blank" rel="noreferrer">
+                            <i className='bx bxl-linkedin'></i>
+                        </a>
+                        <a href="https://github.com/achraftes" className="home__social-icon" target="_blank" rel="noreferrer">
+                            <i className="uil uil-github-alt"></i>
+                        </a>
+                    </div>
+                </Animate>
             </section>
-
-            
         </>
     )
 }
+
 export default Home;
